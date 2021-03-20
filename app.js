43d1d75fff5fc24b8c8ca487c30c5b1e7bc0d2bc
chica25 <patricia@patriciaco.com>
2021-03-20 16:02:37 -0400

    Implement array of quotes

---
+++ app.js
@@ -0,0 +1,51 @@
+let quotes = [
+    {
+        "quote": "I run a small fake ID company from my car with a laminating machine that I swiped from the Sheriff’s station.",
+        "character": "Creed Bratton"
+    },
+
+    {
+        "quote": "The worst thing about prison was the dementors",
+        "character": " Michael 'Prison Mike' Scott"
+    },
+
+    {
+        "quote": "How is it possible that in five years, I’ve had two engagements and only one chair?",
+        "character": "Pam Beesley"
+    },
+
+    {
+        "quote": "It's like I used to tell my wife. I do not apologize unless I think I’m wrong. And if you don’t like it you can leave. And I say the same thing to my current wife and I’ll say it to my next one, too.",
+        "character": "Stanley Hudson"
+    },
+
+    {
+        "quote": "I love inside jokes. I hope to be a part of one someday.",
+        "character": "Michael Scott"
+    },
+
+    {
+        "quote": "For my new year's resolution, I gave up drinking... during the week.",
+        "character": "Meredith Palmer"
+    },
+
+    {
+        "quote": "From time to time I send Dwight faxes. From himself. From the future.",
+        "character": "Jim Halpert"
+    },
+
+    {
+        "quote": "If I had a gun with two bullets and I was in a room with Hitler, Bin Laden, and Toby, I would shoot Toby twice.",
+        "character": "Michael Scott"
+    },
+
+    {
+        "quote": "Pam, come on. 'I like your food?' Come on, stir the pot, stir the melting pot, Pam! Let's do it, let's get ugly, let's get real",
+        "character": "Michael Scott"
+    },
+
+    {
+        "quote": "One day Michael came in and complained about a speed bump on the highway. I wonder who he ran over then.",
+        "character": "Jim Halpert"
+    }
+]
